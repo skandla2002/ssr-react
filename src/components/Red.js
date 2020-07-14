6d8a6b5ea04f8a7da8f3265be5fb1aeaba60a98f
JONGMEONG PAK <skandla2002@gmail.com>
2020-07-14 19:09:50 +0900

    ssr 최초 커밋

---
+++ src/components/Red.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Red.css';
+
+export class Red extends Component {
+    render() {
+        return (
+            <div className="Red">
+                Red
+            </div>
+        )
+    }
+}
+
+
+export default Red
